6e05205396716c13ec8af5f300d299bafe13ab55
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2019-07-21 14:40:42 +0100

    buyAndSellExample

--- test/sbm/LiveOrderBoardTests.java
+++ test/sbm/LiveOrderBoardTests.java
@@ -36,5 +36,28 @@ public class LiveOrderBoardTests {
                         new Price(310), new Quantity("1.2")
                 ))
         );
+        assertThat(orderBoard.summaryOf(BUY), equalTo(emptyMap()));
+    }
+
+    @Test public void buyAndSellExample() {
+        List<Order> orders = List.of(
+                new Order(new UserId("user1"), new Quantity("3.5"), new Price(306), BUY),
+                new Order(new UserId("user2"), new Quantity("1.2"), new Price(310), SELL),
+                new Order(new UserId("user3"), new Quantity("1.5"), new Price(307), BUY),
+                new Order(new UserId("user4"), new Quantity("2.0"), new Price(306), BUY)
+        );
+        orders.forEach(orderBoard::register);
+
+        assertThat(
+                orderBoard.summaryOf(BUY),
+                equalTo(Map.of(
+                        new Price(306), new Quantity("5.5"),
+                        new Price(307), new Quantity("1.5")
+                ))
+        );
+        assertThat(
+                orderBoard.summaryOf(SELL),
+                equalTo(Map.of(new Price(310), new Quantity("1.2")))
+        );
     }
 }
